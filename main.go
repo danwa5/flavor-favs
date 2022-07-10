8edce621e5ea0623df56206f13e475a2f2e9245b
Daniel Lee <danwa5@gmail.com>
2022-07-10 10:57:33 -0700

    Refactor to access playlist data without using spotify library

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
     "context"
+    "encoding/json"
     "fmt"
+    "io/ioutil"
     "log"
+    "net/http"
     "os"
 
     "github.com/joho/godotenv"
@@ -12,19 +15,42 @@ import (
     "golang.org/x/oauth2/clientcredentials"
 )
 
+type Playlist struct {
+    ID          string  `json:"id"`
+    IsPublic    bool    `json:"public"`
+    Name        string  `json:"name"`
+    Description string  `json:"description"`
+}
+
 func main() {
     accessToken := getAccessToken()
-    client := spotify.Authenticator{}.NewClient(accessToken)
+    fmt.Println("Access token: " + accessToken.AccessToken)
 
     // https://open.spotify.com/playlist/37i9dQZEVXcO0WnzMtBl3g?si=9894c660aeb846cb
     playlistID := "37i9dQZEVXcO0WnzMtBl3g"
-    spotifyPlaylistID := spotify.ID(playlistID)
-    playlist, err := client.GetPlaylist(spotifyPlaylistID)
-    handleError(err, "Error fetching playlist " + playlistID)
+
+    url := "https://api.spotify.com/v1/playlists/" + playlistID
+    var bearer = "Bearer " + accessToken.AccessToken
+
+    req, err := http.NewRequest("GET", url, nil)
+    req.Header.Add("Authorization", bearer)
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    handleError(err, "Error making HTTP request")
+    defer resp.Body.Close()
+
+    byteValue, err := ioutil.ReadAll(resp.Body)
+    handleError(err, "Error while reading the response bytes")
+    // log.Println(string([]byte(byteValue)))
+
+    var playlist Playlist
+    json.Unmarshal([]byte(byteValue), &playlist)
 
     fmt.Println("playlist id:", playlist.ID)
     fmt.Println("playlist name:", playlist.Name)
     fmt.Println("playlist description:", playlist.Description)
+    fmt.Println("playlist public:", playlist.IsPublic)
 }
 
 func getAccessToken() (*oauth2.Token) {
